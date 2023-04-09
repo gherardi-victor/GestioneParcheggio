a50f869d5692de896475773b41478741629dadca
gherardi-victor <gherardi.victor.studente@itispaleocapa.it>
2023-04-09 20:52:07 +0200

    aggiunte car alla GUI

--- src/main/java/com/example/gestioneparcheggio/Application.java
+++ src/main/java/com/example/gestioneparcheggio/Application.java
@@ -2,6 +2,8 @@ package com.example.gestioneparcheggio;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
--- src/main/java/com/example/gestioneparcheggio/Controller.java
+++ src/main/java/com/example/gestioneparcheggio/Controller.java
@@ -2,9 +2,22 @@ package com.example.gestioneparcheggio;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
     public Rectangle sbarra_chiusa;
     public Rectangle sbarra_aperta;
+    public Circle led_verde;
+    public Circle led_giallo;
+    public Circle led_rosso;
+
+
+    public ImageView car_red;
+    public ImageView car_yellow;
+    public ImageView car_light_blue;
+    public ImageView car_blue;
+    public ImageView car_green;
+    public ImageView car_purple;
 }
