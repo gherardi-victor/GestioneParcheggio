70ea2e660ffb1845b7ce8f920d4a238ee991ac39
gherardi-victor <gherardi.victor.studente@itispaleocapa.it>
2023-04-09 19:06:30 +0200

    initial commit

---
+++ src/main/java/com/example/gestioneparcheggio/Application.java
@@ -0,0 +1,24 @@
+package com.example.gestioneparcheggio;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Application.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Gestione Parcheggio");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/gestioneparcheggio/Controller.java
@@ -0,0 +1,10 @@
+package com.example.gestioneparcheggio;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class Controller {
+    public Rectangle sbarra_chiusa;
+    public Rectangle sbarra_aperta;
+}
