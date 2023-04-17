50ab236128dc2e2c684d9f4023299b9f27af0d58
gherardi-victor <gherardi.victor.studente@itispaleocapa.it>
2023-04-17 20:45:18 +0200

    bug in uscita delle auto

--- GestioneParcheggio/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.gestioneparcheggio {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.gestioneparcheggio to javafx.fxml;
-    exports com.example.gestioneparcheggio;
-}
--- src/main/java/com/example/gestioneparcheggio/Application.java
+++ src/main/java/com/example/gestioneparcheggio/Application.java
@@ -2,22 +2,20 @@ package com.example.gestioneparcheggio;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Application extends javafx.application.Application {
+    static boolean caricato = false;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Application.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Gestione Parcheggio");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+        caricato = true;
     }
 
     public static void main(String[] args) {
